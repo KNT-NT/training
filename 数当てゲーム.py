e57296d2da857eb953d97eef6f539b63ec1ed9bf
KNT-NT <k.nomoto0517@gmail.com>
2025-04-03 11:45:42 +0900

    Add files via upload

--- 数当てゲーム.py
+++ 数当てゲーム.py
@@ -1,12 +1,12 @@
 import random
 
 def game(choose_num):
-    correct_num = random.randint(1, 101) #1から100までの数字で数当てゲーム。
-    if choose_num == correct_num:
+    correct_num = random.randint(1, 101) #1から100までの数字で数当てゲーム
+    if choose_num < correct_num: #入力した数字が答えより大きい時
         print("よりも小さいよ")
-    elif choose_num >= correct_num:
+    elif choose_num > correct_num: #入力した数字が答えより小さい時
         print("よりも小さいよ")
-    else:
+    else: #入力した数字と正解が一致したとき
         print("正解！")
 
 game(int(input("数字を入力してください")))
